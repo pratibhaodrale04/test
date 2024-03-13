48e9205c86371c2dd2739e16a10e674bd5ee92f6
pratibhaodrale04 <pratibha.odrale10@gmail.com>
2024-03-13 11:36:53 +0000

    3-created UserInfoManagerConfig class to retrieve the user information from DB

---
+++ security-oauth-jwt-asymmetric/src/main/java/com/val/config/UserInfoManagerConfig.java
@@ -0,0 +1,23 @@
+package com.val.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.val.repo.IUserInfoRepo;
+
+@Service
+public class UserInfoManagerConfig implements UserDetailsService {
+
+	@Autowired
+	IUserInfoRepo userInfoRepo;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return userInfoRepo.findByEmail(username).map(UserInfoConfig::new)
+				.orElseThrow(() -> new UsernameNotFoundException("User Email-->" + username + "does not exist"));
+	}
+
+}
