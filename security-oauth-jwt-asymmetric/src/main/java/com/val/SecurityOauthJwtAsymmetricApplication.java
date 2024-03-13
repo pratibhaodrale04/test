88012b331e3052fc7a9184115c77d9bf50347347
pratibhaodrale04 <pratibha.odrale10@gmail.com>
2024-03-13 09:32:50 +0000

    developing spring security with oauth2+JWT-asymmetric

---
+++ security-oauth-jwt-asymmetric/src/main/java/com/val/SecurityOauthJwtAsymmetricApplication.java
@@ -0,0 +1,13 @@
+package com.val;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOauthJwtAsymmetricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityOauthJwtAsymmetricApplication.class, args);
+	}
+
+}
