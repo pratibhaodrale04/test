906a67c5e9e03505073f1d2a27943f42ac747383
pratibhaodrale04 <pratibha.odrale10@gmail.com>
2024-03-13 21:54:47 +0000

    8.Created certs with asymmetric(public&private) to return JWT token while authenticating and .yml changes

--- security-oauth-jwt-asymmetric/src/main/java/com/val/SecurityOauthJwtAsymmetricApplication.java
+++ security-oauth-jwt-asymmetric/src/main/java/com/val/SecurityOauthJwtAsymmetricApplication.java
@@ -2,7 +2,11 @@ package com.val;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.val.config.RSA.RSAKeyRecord;
+
+@EnableConfigurationProperties(RSAKeyRecord.class)
 @SpringBootApplication
 public class SecurityOauthJwtAsymmetricApplication {
 
